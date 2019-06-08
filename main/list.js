54dc010a09eb85b9c33baecb975b3a661c2ae0e7
Aigeros <37756642+Aigeros@users.noreply.github.com>
2019-06-08 22:40:10 +0200

    Add files via upload

--- main/list.js
+++ main/list.js
@@ -1,1 +1,30 @@
-'use strict';var DC=DC||{};DC.blocklist={};DC.blocklistSubdomain=[];DC.load=function(){DC.blocklist=localStorage.hasOwnProperty('blockLIST')?JSON.parse(localStorage.getItem('blockLIST')):{};var k;DC.blocklistSubdomain=[];for(k in DC.blocklist){if(DC.blocklist.hasOwnProperty(k)){if(k.substr(0,2)==='*.'){DC.blocklistSubdomain.push(k.substr(1));}}}};DC.save=function(){localStorage.setItem('blockLIST',JSON.stringify(DC.blocklist));};DC.load();
+// persistent blocklist
+'use strict';
+var DC = DC || {};
+
+// list of blocked hostnames or urls
+DC.blocklist = {};
+
+// list of blocked subdomains
+DC.blocklistSubdomain = [];
+
+// load blocklist from local storage
+DC.load = function () {
+	// extract entries with asterisks (*.example.com)
+	DC.blocklist = localStorage.hasOwnProperty('blockLIST') ? JSON.parse(localStorage.getItem('blockLIST')) : {};
+	var k;
+	DC.blocklistSubdomain = [];
+	for (k in DC.blocklist) {
+		if (DC.blocklist.hasOwnProperty(k)) {
+			if (k.substr(0, 2) === '*.') {
+				DC.blocklistSubdomain.push(k.substr(1));
+			}
+		}
+	}
+};
+
+// save blocklist to local storage
+DC.save = function () {
+	localStorage.setItem('blockLIST', JSON.stringify(DC.blocklist));
+};
+DC.load();
--- main/options.js
+++ main/options.js
@@ -1,2 +1,26 @@
-'use strict';var DC=DC||{};window.addEventListener('DOMContentLoaded',function(){DC.tb=document.getElementById('blocklist');DC.tb.value=Object.keys(DC.blocklist).sort().join('\n');document.getElementById('save').addEventListener('click',function(){var a,i;DC.blocklist={};a=DC.tb.value.trim().split('\n');for(i=0;i<a.length;i++){DC.blocklist[a[i]]=1;}
-DC.save();chrome.runtime.sendMessage({cmd:'reload'});});});
+// options callbacks
+'use strict';
+var DC = DC || {};
+
+// show list
+window.addEventListener('DOMContentLoaded', function () {
+	// sort list
+	DC.tb = document.getElementById('blocklist');
+	DC.tb.value = Object.keys(DC.blocklist).sort().join('\n');
+	// save list
+	document.getElementById('save').addEventListener('click', function () {
+		var a,
+		i;
+		// prepare list
+		DC.blocklist = {};
+		a = DC.tb.value.trim().split('\n');
+		for (i = 0; i < a.length; i++) {
+			DC.blocklist[a[i]] = 1;
+		}
+		// tell background script to reload list
+		DC.save();
+		chrome.runtime.sendMessage({
+			cmd: 'reload'
+		});
+	});
+});
