00f1c893ca96634db83ee8cda25239d5c2c080c7
Aigeros <37756642+Aigeros@users.noreply.github.com>
2019-06-08 22:36:41 +0200

    Add files via upload

--- main/options.js
+++ main/options.js
@@ -1,26 +1,2 @@
-// options callbacks
-'use strict';
-var DC = DC || {};
-
-// show list
-window.addEventListener('DOMContentLoaded', function () {
-	// sort list
-	DC.tb = document.getElementById('blocklist');
-	DC.tb.value = Object.keys(DC.blocklist).sort().join('\n');
-	// save list
-	document.getElementById('save').addEventListener('click', function () {
-		var a,
-		i;
-		// prepare list
-		DC.blocklist = {};
-		a = DC.tb.value.trim().split('\n');
-		for (i = 0; i < a.length; i++) {
-			DC.blocklist[a[i]] = 1;
-		}
-		// tell background script to reload list
-		DC.save();
-		chrome.runtime.sendMessage({
-			cmd: 'reload'
-		});
-	});
-});
+'use strict';var DC=DC||{};window.addEventListener('DOMContentLoaded',function(){DC.tb=document.getElementById('blocklist');DC.tb.value=Object.keys(DC.blocklist).sort().join('\n');document.getElementById('save').addEventListener('click',function(){var a,i;DC.blocklist={};a=DC.tb.value.trim().split('\n');for(i=0;i<a.length;i++){DC.blocklist[a[i]]=1;}
+DC.save();chrome.runtime.sendMessage({cmd:'reload'});});});
